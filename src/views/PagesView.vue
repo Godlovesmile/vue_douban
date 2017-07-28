0df3ed88dc4364ddaa8ac353d6f6119974e54ee5
Alice <2272674323@qq.com>
2017-07-28 08:58:51 +0800

    route configuration

---
+++ src/views/PagesView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderBar/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import HeaderBar from '../components/HeaderBar.vue'
+  export default {
+    data () {
+      return {}
+    },
+    components: {
+      HeaderBar
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
